663ef35ac1c59849473df6b4bb9f46662d995c43
Juan Pablo Marin <jpmarint@gmail.com>
2023-01-11 13:15:36 -0500

    Docker file added and fixed

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,20 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+
+app =  FastAPI()
+
+@app.get('/')
+def get_list():
+    return[1,2,3,4]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_company():
+    return """
+        <h1>Hello, I'm a website</h1>
+        <p>Company name: JP Morgan</p>
+    """
 
 def run():
     store.get_categories()
